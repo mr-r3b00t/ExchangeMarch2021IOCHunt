1e97ca97e90e19d0489a611c253708fa745a6fc7
Daniel Card <dan.card@xservus.com>
2021-03-03 09:37:16 +0000

    Create healthcheck.ps1

---
+++ healthcheck.ps1
@@ -0,0 +1,12 @@
+#Runs the microsoft exchange health check script
+#just speeds this process up
+
+#Requires -RunAsAdministrator
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$TLS12Protocol = [System.Net.SecurityProtocolType] 'Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
+
+wget https://github.com/dpaulson45/HealthChecker/releases/download/v3.3.1/HealthChecker.ps1 -OutFile HealthChecker.ps1
+
+.\HealthChecker.ps1
