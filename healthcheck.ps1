64346ab2dd48f34dae3f4b02b20062f4965fddc0
Daniel Müller <daniel.mueller@cobotec.de>
2021-03-04 16:16:59 +0100

    Update healthcheck.ps1
    updated URL to newer version (with security check update)
    added HTML file output

--- healthcheck.ps1
+++ healthcheck.ps1
@@ -7,6 +7,7 @@ Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 $TLS12Protocol = [System.Net.SecurityProtocolType] 'Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
 
-wget https://github.com/dpaulson45/HealthChecker/releases/download/v3.3.1/HealthChecker.ps1 -OutFile HealthChecker.ps1
+wget https://github.com/dpaulson45/HealthChecker/releases/download/v3.3.2/HealthChecker.ps1 -OutFile HealthChecker.ps1
 
 .\HealthChecker.ps1
+.\HealthChecker.ps1 -BuildHtmlServersReport
