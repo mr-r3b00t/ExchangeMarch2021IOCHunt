f97e1696808e8ee843a63aff37078fffbf06e620
Daniel Card <dan.card@xservus.com>
2021-03-08 10:17:51 +0000

    Update fastcheck.ps1
    there are two hard coded paths still

--- fastcheck.ps1
+++ fastcheck.ps1
@@ -238,7 +238,7 @@ foreach($file in $files)
 
     
 $badhash = "b75f163ca9b9240bf4b37ad92bc7556b40a17e27c2b8ed5c8991385fe07d17d0","097549cf7d0f76f0d99edf8b2d91c60977fd6a96e4b8c3c94b0b1733dc026d3e","2b6f1ebb2208e93ade4a6424555d6a8341fd6d9f60c25e44afe11008f5c1aad1","65149e036fff06026d80ac9ad4d156332822dc93142cf1a122b1841ec8de34b5","511df0e2df9bfa5521b588cc4bb5f8c5a321801b803394ebc493db1ef3c78fa1","4edc7770464a14f54d17f36dc9d0fe854f68b346b27b35a6f5839adf1f13f8ea","811157f9c7003ba8d17b45eb3cf09bef2cecd2701cedb675274949296a6a183d","1631a90eb5395c4e19c7dbcbf611bbe6444ff312eb7937e286e4637cb9e72944"
-Write-host "Checking C:\inetpub\wwwroot\aspnet_client for extra files"
+Write-host "Checking inetpub\wwwroot\aspnet_client for extra files"
 $enumfiles = Get-ChildItem -Path $newpath -filter "*.aspx" -Recurse -File
 foreach($file in $enumfiles){
 write-host $file.DirectoryName
@@ -270,6 +270,7 @@ else
 #read all the IIS logs looking for POST requests to /owa/auth/Current/themes/resources/
 Write-host "Checking for theme resource indicators"
 
+#FIX THIS HARD CODED PATH
 $parse1 = Select-String -Path "C:\inetpub\logs\LogFiles\W3SVC1\*.log" -Pattern 'POST /owa/auth/Current/themes/resources/'
 
 foreach($line in $parse1){
